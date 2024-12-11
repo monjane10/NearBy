90a4199ffecbcbfceb59313dbe56835f84d7a755
monjane10 <louramonja52.n@gmail.com>
2024-12-11 10:04:05 +0200

    puxando places da api

--- src/components/places/index.tsx
+++ src/components/places/index.tsx
@@ -1,13 +1,28 @@
-import {View} from "react-native";
-import {s} from "./styles";
-import{Place, PlaceProps} from "../place";
-
+import React, { useRef } from "react";
+import { Text, useWindowDimensions } from "react-native";
+import { s } from "./styles";
+import { Place, PlaceProps } from "../place";
+import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
 
 type Props = {
-    data: PlaceProps[]
-}
+  data: PlaceProps[];
+};
 
-export function Places({data}: Props) {
-    return <View></View>
-    
+export function Places({ data }: Props) {
+  const dimensions = useWindowDimensions();
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  return (
+    <BottomSheet
+      ref={bottomSheetRef}
+      index={0} 
+      snapPoints={[dimensions.height * 0.5, dimensions.height * 0.75, "100%"]} 
+    >
+      <BottomSheetFlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()} 
+        renderItem={({ item }) => <Place data={item} />}
+      />
+    </BottomSheet>
+  );
 }
