48786647a2026dabd2919f3b259bdfcd6cdc51d1
monjane10 <louramonja52.n@gmail.com>
2024-12-11 08:46:29 +0200

    adicionando estilos as categorias

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,6 +1,7 @@
 import {Text, Pressable,PressableProps} from "react-native";
 import {s} from "./styles"
 import { categoriesIcons } from "@/utils/categories-icons";
+import {colors} from "@/styles/theme";
 
 type Props = PressableProps & {
     iconId: string;
@@ -11,9 +12,9 @@ type Props = PressableProps & {
 export function Category({iconId, isSelected, name, ...rest}: Props) {
     const Icon = categoriesIcons[iconId]
     return (
-        <Pressable style={[s.container]}>
-            <Icon size={16} />
-            <Text style={[s.name]}>{name}</Text>
+        <Pressable style={[s.container, isSelected && s.containerSelected]} {...rest}>
+            <Icon size={16} color={colors.gray[isSelected ? 100 : 400]} />
+            <Text style={[s.name, isSelected && s.nameSelected]}>{name}</Text>
         </Pressable>
     )
 }
