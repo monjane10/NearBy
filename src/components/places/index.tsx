d3fb0ca6c776c6f8459798b1858dad8506046e26
monjane10 <louramonja52.n@gmail.com>
2024-12-15 09:25:53 +0200

    actualizacoes

--- src/components/places/index.tsx
+++ src/components/places/index.tsx
@@ -3,6 +3,7 @@ import { Text, useWindowDimensions } from "react-native";
 import { s } from "./styles";
 import { Place, PlaceProps } from "../place";
 import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
+import { router } from "expo-router";
 
 type Props = {
   data: PlaceProps[];
@@ -11,24 +12,30 @@ type Props = {
 export function Places({ data }: Props) {
   const dimensions = useWindowDimensions();
   const bottomSheetRef = useRef<BottomSheet>(null);
+
   const snapPoints = {
     min: 278,
     max: dimensions.height - 128,
-  }
+  };
 
   return (
     <BottomSheet
       ref={bottomSheetRef}
-      index={0} 
-      snapPoints={[snapPoints.min, snapPoints.max]} 
+      index={0}
+      snapPoints={[snapPoints.min, snapPoints.max]}
       handleIndicatorStyle={s.indicator}
       backgroundStyle={s.container}
       enableOverDrag={false}
     >
       <BottomSheetFlatList
         data={data}
-        keyExtractor={(item) => item.id.toString()} 
-        renderItem={({ item }) => <Place data={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Place
+            data={item}
+            onPress={() => router.push({ pathname: '/market/[id]', params: { id: item.id } })} // Passando o parâmetro corretamente
+          />
+        )}
         ListHeaderComponent={() => <Text style={s.title}>Explore lugares perto de ti</Text>}
         showsVerticalScrollIndicator={false}
       />
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.232.227:3333",
+  baseURL: "http://192.144.173:3333",
   timeout: 700,
 })
